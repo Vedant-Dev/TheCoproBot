fcf77dd6f685e2eb5be66e5f01fc4da036307643
Vedant <iamsinghved@gmail.com>
2021-03-20 21:16:16 +0530

    Added feature of all types of message

--- components.py
+++ components.py
@@ -20,21 +20,21 @@ class Chat:
 		self.discription = discription
 
 class Message:
-	def __init__(self,message_id,from_user=None,sender_chat=None,date=None,chat=None,
+	def __init__(self,message_id,from_=None,sender_chat=None,date=None,chat=None,
 		reply_to_message=None,text=None,animation=None,audio=None,document=None,
 		photo=None,sticker=None,video=None, *args, **kwargs):
 		self.message_id = message_id
-		self.from_user = from_user
-		self.sender_chat = sender_chat
+		self.from_ = User(**from_)
+		self.sender_chat = Chat(**sender_chat) if sender_chat else None
 		self.date = date
 		self.reply_to_message = reply_to_message
 		self.text = text
-		self.animation = animation
-		self.audio = audio
-		self.document = document
-		self.photo = photo
-		self.sticker = sticker
-		self.video = video
+		self.animation = Animation(**animation) if animation else None
+		self.audio = Audio(**audio) if audio else None
+		self.document = Document(**document) if document else None
+		self.photo = [PhotoSize(**p) for p in photo if p] if photo else None 
+		self.sticker = Sticker(**sticker) if sticker else None
+		self.video = Video(**video) if video else None
 class MessageId:
 	def __init__(self,message_id, *args, **kwargs):
 		self.message_id = message_id
@@ -55,7 +55,7 @@ class Animation:
 		self.width = width
 		self.height = height
 		self.duration = duration
-		self.thumb = thumb
+		self.thumb = PhotoSize(**thumb) if thumb else None
 		self.file_name = file_name
 		self.mime_type = mime_type
 		self.file_size = file_size
@@ -68,7 +68,7 @@ class Audio:
 		self.duration = duration
 		self.performer = performer
 		self.title = title
-		self.thumb = thumb
+		self.thumb = PhotoSize(**thumb) if thumb else None
 		self.file_name = file_name
 		self.mime_type = mime_type
 		self.file_size = file_size
@@ -77,7 +77,7 @@ class Document:
 	def __init__(self,file_id,file_unique_id,thumb=None,file_name=None,mime_type=None,file_size=None, *args, **kwargs):
 		self.file_id = file_id
 		self.file_unique_id = file_unique_id
-		self.thumb = thumb
+		self.thumb = PhotoSize(**thumb) if thumb else None
 		self.file_name = file_name
 		self.mime_type = mime_type
 		self.file_size = file_size
@@ -90,7 +90,32 @@ class Video:
 		self.width = width
 		self.height = height
 		self.duration = duration
-		self.thumb = thumb
+		self.thumb = PhotoSize(**thumb) if thumb else None
 		self.file_name = file_name
 		self.mime_type = mime_type
 		self.file_size = file_size
+
+class Sticker:
+	def __init__(self,file_id,file_unique_id,width,height,is_animated,
+		thumb=None,emoji=None,set_name=None,file_size=None, *args, **kwargs):
+		self.file_id = file_id
+		self.file_unique_id = file_unique_id
+		self.width = width
+		self.height = height
+		self.is_animated = is_animated
+		self.thumb = PhotoSize(**thumb) if thumb else None
+		self.emoji = emoji
+		self.set_name = set_name
+		self.file_size = file_size
+
+class BotCommand:
+	def __init__(self,command,discription):
+		self.command = command
+		self.discription = discription
+
+class ChatPhoto:
+	def __init__(self,small_file_id,small_file_unique_id,big_file_id,big_file_unique_id):
+		self.small_file_id = small_file_id
+		self.small_file_unique_id = small_file_unique_id
+		self.big_file_id = big_file_id
+		self.big_file_unique_id = big_file_unique_id
--- server.py
+++ server.py
@@ -2,14 +2,27 @@ from bot import TelegramBot as Bot
 import bs4
 import requests
 import re
+import json
+from components import Message
 
 bot = Bot(config='config.py')
 
 def send_reply(message):
 	if not message:
 		return 'Message to aaya hi nahi'
-	return gangsterize(message)
-
+	if message.animation is not None:
+		return 'U send a animation'
+	if message.audio is not None:
+		return 'U send a audio'
+	if message.video is not None:
+		return 'U send a video'
+	if message.document is not None:
+		return 'U send a document'
+	if message.sticker is not None:
+		return 'u send a sticker'
+	if message.photo is not None:
+		return 'u send a photo'
+	return gangsterize(message.text)
 def gangsterize(input_text):
 	params = {"translatetext": input_text}
 	target_url = "http://www.gizoogle.net/textilizer.php"
@@ -20,6 +33,7 @@ def gangsterize(input_text):
 	giz = soup.find_all(text=True)
 	giz_text = giz[37].strip("\r\n")  # Hacky, but consistent.
 	return giz_text
+
 update_id = -1
 while True:
 	updates = bot.getUpdates(offset=(update_id + 1))
@@ -27,10 +41,10 @@ while True:
 	if result:
 		for item in result:
 			update_id = item['update_id']
-			try:
-				message = item['message']['text']
-			except:
-				message = None
-			user_id = item['message']['from']['id']
-			bot.sendMessage(message=send_reply(message),chat_id=user_id)
+			if 'message' in item:
+				message = Message(**item['message'],from_=item['message']['from'])
+				bot.sendTextMessage(message=send_reply(message),chat_id=message.from_.id)
+			else:
+				message = Message(**item['edited_message'],from_=item['edited_message']['from'])
+				bot.sendTextMessage(message=send_reply(message),chat_id=message.from_.id,reply_to_message_id=message.message_id)
 
